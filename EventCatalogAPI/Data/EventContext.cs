fe496a28186fd5f205a71fed27e3af15ab554014
Mugdha Pati <mugdhapati@gmail.com>
2020-06-11 12:37:42 -0700

    Initial commit

---
+++ EventCatalogAPI/Data/EventContext.cs
@@ -0,0 +1,21 @@
+﻿using EventCatalogAPI.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventCatalogAPI.Data
+{
+    public class EventContext : DbContext              //purpose of creating this class is to define my store
+    {
+        public DbSet<EventType> EventTypes { get; set; }           //DbSet is a DB table
+        public DbSet<EventLocation> EventLocations { get; set; }   //Classes Eventtype,iten, location are changed into Tables
+        public DbSet<EventItem> EventItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+    }
+}   
---
+++ EventCatalogAPI/Domain/EventItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventCatalogAPI.Domain
+{
+    public class EventItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Location { get; set; }
+        public DateTime DateTime { get; set; }
+        public decimal Price { get; set; }
+        public string PictureUrl { get; set; }
+        public int EventTypeId { get; set; }
+        public int EventLocationId { get; set; }    
+
+        public EventType EventType { get; set; }
+        public EventLocation EventLocation { get; set; }
+    }   
+}
