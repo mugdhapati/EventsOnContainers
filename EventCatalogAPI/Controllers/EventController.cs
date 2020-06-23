54b5225d2422c9b43f7ec1b930891f45829f2c7b
Mugdha Pati <mugdhapati@gmail.com>
2020-06-23 04:52:38 -0700

    Controllers Commit

---
+++ EventCatalogAPI/Controllers/EventController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EventCatalogAPI.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventCatalogAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventController : ControllerBase
+    {
+        private readonly EventContext _context;
+        public EventController(EventContext context)
+        {
+            _context = context;
+        }
+        [HttpGet("[action]")]
+        public async Task<IActionResult>Items(
+            [FromQuery]int pageIndex = 0,
+            [FromQuery] int pagesize = 6)
+        {
+            var items = await _context.EventItems
+                    .OrderBy(e => e.Name)
+                    .Skip(pageIndex * pagesize)
+                    .Take(pagesize)
+                    .ToListAsync();
+
+            return Ok(items);
+        }
+    }
+}
--- EventCatalogAPI/Data/EventSeed.cs
+++ EventCatalogAPI/Data/EventSeed.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace EventCatalogAPI.Data
 {
-    public class EventSeed
+    public static class EventSeed
     {
         public static void Seed(EventContext eventContext)
         {
@@ -25,7 +25,8 @@ namespace EventCatalogAPI.Data
             }
             if (!eventContext.EventItems.Any())
             {
-
+                eventContext.EventItems.AddRange(GetEventItems());
+                eventContext.SaveChanges();
             }
         }
         private static IEnumerable<EventCategory> GetEventCategories()
@@ -43,7 +44,7 @@ namespace EventCatalogAPI.Data
                 new EventCategory
                 {
                     Category= "Sports & Fitness"
-                },
+                }
             };
         }
         private static IEnumerable<EventType> GetEventTypes()
@@ -68,21 +69,21 @@ namespace EventCatalogAPI.Data
         {
             return new List<EventItem>
             {
-                new EventItem { EventTypeId = 1, EventCategoryId = 1, Name = "BE About IT", Description = "Be About It: Unpacking White Privilege, Bias, and Anti-Racist Instruction", Location = "Online Event", DateTime = , Price = 60, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/1" },
-                new EventItem { EventTypeId = 1, EventCategoryId = 2, Name= "Webinar Statistika", Description = "Webinar Statistika #3 | Statistical Methods (ANOVA and MANOVA)", Location = "Online Event", DateTime= , Price = 55, PictureUrl= "http://externalcatalogbaseurltobereplaced/api/pic/2" },
-                new EventItem { EventTypeId = 3, EventCategoryId = 2, Name= "ProductCon Online", Description = "ProductCon Online: The Product Management Conference", Location = "Online Event", DateTime=, Price = 150, PictureUrl= "http://externalcatalogbaseurltobereplaced/api/pic/3" },
-                new EventItem { EventTypeId = 2, EventCategoryId = 3, Name = "2020 NJCAAE", Description = "2020 NJCAAE Convention Day 1", Location = "Online Event", DateTime = , Price = 0, PictureUrl= "http://externalcatalogbaseurltobereplaced/api/pic/4"  },
-                new EventItem { EventTypeId = 1, EventCategoryId = 2, Name = "Biomarkers", Description = "Use of Biomarkers to Establish the Presence and Severity of Impairment", Location = "Online Event", DateTime= ,Price = 30, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/5"},
-                new EventItem { EventTypeId = 2, EventCategoryId = 1, Name = "PSCC 2020", Description = "XXI Power Systems Computation Conference", Location = "Online Event", DateTime = ,Price = 100, PictureUrl="http://externalcatalogbaseurltobereplaced/api/pic/6"},
-                new EventItem { EventTypeId = 3, EventCategoryId = 1, Name = "RELIT!", Description = "RELIT! 2020: Bring. Your. Brave.", Location = "Online Event", DateTime = , Price = 50, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/7"},
-                new EventItem { EventTypeId = 1, EventCategoryId = 3, Name = "The Coronavirus Advice", Description = "Webinar: The Coronavirus Advice Your Fitness Business Needs to Hear", Location = "Online Event", DateTime = , Price = 60, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/8"},
-                new EventItem { EventTypeId = 3, EventCategoryId = 3, Name = "SportBite", Description = "Introduction to Elite Performance Conference - SportBite", Location = "Online Event", DateTime = ,Price = 10, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/9"},
-                new EventItem { EventTypeId = 2, EventCategoryId = 2, Name = "Miami 2020", Description = "Mining Disrupt Conference LIVE | Bitcoin Blockchain Cryptocurrency Mining", Location = "Online Event", DateTime = , Price = 100, PictureUrl= "http://externalcatalogbaseurltobereplaced/api/pic/10"},
-                new EventItem { EventTypeId = 1, EventCategoryId = 1, Name = "NABME", Description = "A Conversation to Foster Anti-Racist Practices in Schools", Location = "Online Event", DateTime = , Price = 5, PictureUrl= "http://externalcatalogbaseurltobereplaced/api/pic/11" },
-                new EventItem { EventTypeId = 3, EventCategoryId = 1, Name = "The 2020 9th Annual", Description = "2020 9th Annual Liberated Minds Black Homeschool", Location = "Online Event", DateTime = , Price = 25, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/12"},
-                new EventItem { EventTypeId = 2, EventCategoryId = 2, Name = "HHL Expert Talk", Description = "HHL Expert Talk - How to become a great negotiator? Research and practice", Location = "Online Event", DateTime = , Price = 30, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/13" },
-                new EventItem { EventTypeId = 1, EventCategoryId = 1, Name = "Back to School", Description = "Webinar: Business and Engineering in the USA", Location = "Online Event", DateTime = , Price = 0, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/14"},
-                new EventItem { EventTypeId = 3, EventCategoryId = 2, Name = "New Era", Description = "A New Era for Food and Climate", Location = "Online Event", DateTime =, Price = 45, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/15" },
+                new EventItem { EventTypeId = 1, EventCategoryId = 1, Name = "BE About IT", Description = "Be About It: Unpacking White Privilege, Bias, and Anti-Racist Instruction", Location = "Redmond Convention Center",  Price = 60, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/1" },
+                new EventItem { EventTypeId = 1, EventCategoryId = 2, Name= "Webinar Statistika", Description = "Webinar Statistika #3 | Statistical Methods (ANOVA and MANOVA)", Location = "Online", Price = 55, PictureUrl= "http://externalcatalogbaseurltobereplaced/api/pic/2" },
+                new EventItem { EventTypeId = 3, EventCategoryId = 2, Name= "ProductCon Online", Description = "ProductCon Online: The Product Management Conference", Location = "Online Event", Price = 150, PictureUrl= "http://externalcatalogbaseurltobereplaced/api/pic/3" },
+                new EventItem { EventTypeId = 2, EventCategoryId = 3, Name = "2020 NJCAAE", Description = "2020 NJCAAE Convention Day 1", Location = "Online Event",  Price = 0, PictureUrl= "http://externalcatalogbaseurltobereplaced/api/pic/4"  },
+                new EventItem { EventTypeId = 1, EventCategoryId = 2, Name = "Biomarkers", Description = "Use of Biomarkers to Establish the Presence and Severity of Impairment", Location = "Online Event", Price = 30, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/5"},
+                new EventItem { EventTypeId = 2, EventCategoryId = 1, Name = "PSCC 2020", Description = "XXI Power Systems Computation Conference", Location = "Online Event", Price = 100, PictureUrl="http://externalcatalogbaseurltobereplaced/api/pic/6"},
+                new EventItem { EventTypeId = 3, EventCategoryId = 1, Name = "RELIT!", Description = "RELIT! 2020: Bring. Your. Brave.", Location = "Online Event", Price = 50, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/7"},
+                new EventItem { EventTypeId = 1, EventCategoryId = 3, Name = "The Coronavirus Advice", Description = "Webinar: The Coronavirus Advice Your Fitness Business Needs to Hear", Location = "Online Event",  Price = 60, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/8"},
+                new EventItem { EventTypeId = 3, EventCategoryId = 3, Name = "SportBite", Description = "Introduction to Elite Performance Conference - SportBite", Location = "Online Event", Price = 10, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/9"},
+                new EventItem { EventTypeId = 2, EventCategoryId = 2, Name = "Miami 2020", Description = "Mining Disrupt Conference LIVE | Bitcoin Blockchain Cryptocurrency Mining", Location = "Online Event",  Price = 100, PictureUrl= "http://externalcatalogbaseurltobereplaced/api/pic/10"},
+                new EventItem { EventTypeId = 1, EventCategoryId = 1, Name = "NABME", Description = "A Conversation to Foster Anti-Racist Practices in Schools", Location = "Online Event",  Price = 5, PictureUrl= "http://externalcatalogbaseurltobereplaced/api/pic/11" },
+                new EventItem { EventTypeId = 3, EventCategoryId = 1, Name = "The 2020 9th Annual", Description = "2020 9th Annual Liberated Minds Black Homeschool", Location = "Online Event",  Price = 25, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/12"},
+                new EventItem { EventTypeId = 2, EventCategoryId = 2, Name = "HHL Expert Talk", Description = "HHL Expert Talk - How to become a great negotiator? Research and practice", Location = "Online Event",  Price = 30, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/13" },
+                new EventItem { EventTypeId = 1, EventCategoryId = 1, Name = "Back to School", Description = "Webinar: Business and Engineering in the USA", Location = "Online Event", Price = 0, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/14"},
+                new EventItem { EventTypeId = 3, EventCategoryId = 2, Name = "New Era", Description = "A New Era for Food and Climate", Location = "Online Event",  Price = 45, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/15" }
 
             };
         }
